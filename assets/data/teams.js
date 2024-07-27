8ab3f2854b3fd248e163a38068d11538a8fa1dd8
Ajay Kumar <ajthakur1205@gmail.com>
2024-07-27 15:12:23 +0530

    Update: Added 2026 team data

--- assets/data/teams.js
+++ assets/data/teams.js
@@ -1,5 +1,6 @@
 import t1 from "@/assets/data/teams/2024.json";
 import t2 from "@/assets/data/teams/2025.json";
+import t3 from "@/assets/data/teams/2026.json";
 
 const teams = [
   {
@@ -9,6 +10,10 @@ const teams = [
   {
     batch: "2025",
     data: t2,
+  },
+  {
+    batch: "2026",
+    data: t3,
   },
 ];
 
